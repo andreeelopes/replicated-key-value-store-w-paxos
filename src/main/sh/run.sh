274b19e2e40129e6dcba743490c8007b20146a92
Nelson Coquenim <n.coquenim@campus.fct.unl.pt>
2018-12-11 12:19:30 +0000

    Added a script to help running clients and replicas

--- src/main/sh/run.sh
+++ src/main/sh/run.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
-nNodes=$1
-ip=$2
-port=$3
+nClients=$1
+nReplicas=$2
 
-echo "deploying Rendezvous at 127.0.0.1:69"
-java -cp replicated-key-value-store-w-paxos-assembly-0.1 127.0.0.1 69 > rendezvous.log 2 &
+mkdir -p logs/clients
+mkdir -p logs/replicas
+
+echo "clients = $nClients | replicas = $nReplicas"
+
+clientStartPort=70
+replicasStartPort=100
+
+echo "deploying Rendezvous at 127.0.0.1 : 69"
+java -cp replicated-key-value-store-w-paxos-assembly-0.1 $nReplicas > logs/rendezvous.log &
 
 for i in $(seq 1 $nClients)
 do
-    java -jar replicated-key-value-store-w-paxos-assembly-0.1 127.0.0.1
+    echo "deploying Client $i at 127.0.0.1 : $(($clientStartPort+i))"
+    java -cp replicated-key-value-store-w-paxos-assembly-0.1 127.0.0.1 $((clientStartPort+i)) > logs/clients/client$((clientStartPort+i)).log &
 done
 
-for run in $(seq 2 $nNodes)
+
+
+for j in $(seq 1 $nReplicas)
 do
-	let myPort=$((port+run))
-	echo "deploying Node $run at $ip : $myPort"
-	java -jar ./pub-sub-HyParView-assembly-0.1.jar $ip $myPort $ip $port > out$myPort.log 2 &
+	echo "deploying Replica $j at 127.0.0.1 : $((replicasStartPort+j))"
+	java -cp replicated-key-value-store-w-paxos-assembly-0.1 127.0.0.1 $((replicasStartPort+j)) > logs/replicas/replica$((replicasStartPort+j)).log &
 done
